3b4964f44c7229cc4e7997cd4ff65dd1fb924601
avital kucher <avitalk22@gmail.com>
2022-09-21 12:36:45 +0300

    question num list and journal functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import screen
 import time
 import consts
 import journal
+pygame.font.init()
 
 
 def main():
@@ -10,8 +11,11 @@ def main():
     screen.draw_window()
     clock = pygame.time.Clock()
     run = True
+    started = False
     while run:
         clock.tick(consts.FPS)
+        if not started:
+            screen.draw_starting_message()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
