73c5de6013e9a38528c95c73637b761971d177c0
avital kucher <avitalk22@gmail.com>
2022-09-21 11:40:22 +0300

    white screen background

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 import screen
+import time
+import consts
+import journal
 
 
 def main():
     pygame.init()
     screen.draw_window()
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(consts.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            screen.draw_window()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    screen.draw_questions_background()
 
     pygame.quit()
 
