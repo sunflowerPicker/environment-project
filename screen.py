eaff59cff5276abc07f5f0afe0ec4ebf70551c62
avital kucher <avitalk22@gmail.com>
2022-09-21 12:01:58 +0300

    draw message func

--- screen.py
+++ screen.py
@@ -3,10 +3,22 @@ import consts
 
 window = pygame.display.set_mode((consts.WIN_WIDTH, consts.WIN_HEIGHT))
 
+def draw_starting_message():
+    draw_message(consts.START_MESSAGE_1, consts.START_FONT_SIZE,
+                 consts.START_COLOR, consts.START1_LOCATION)
+    draw_message(consts.START_MESSAGE_2, consts.START_FONT_SIZE,
+                 consts.START_COLOR, consts.START2_LOCATION)
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    window.blit(text_img, location)
+    pygame.display.update()
+
 
 def draw_window():
     pygame.display.set_caption("Journal")
-    window.fill(consts.WHITE)
+    window.fill(consts.BLACK)
     pygame.display.update()
 
 
