7da6b7fd03d43008e97b593f75276588f6752717
avital kucher <avitalk22@gmail.com>
2022-09-21 12:07:36 +0300

    added constants

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import pygame.font
+
 WIN_HEIGHT = 500
 WIN_WIDTH = 1000
 
@@ -5,3 +7,11 @@ FPS = 60
 
 WHITE = (255, 255, 255)
 BLACK = (10, 10, 10)
+
+FONT_NAME = 'calibri'
+START_MESSAGE_1 = "Welcome to your journal, are you ready to enter your actions for the day?"
+START_MESSAGE_2 = "Press Enter to start!"
+START_FONT_SIZE = int(0.025 * WIN_WIDTH)
+START_COLOR = WHITE
+START1_LOCATION = (WIN_WIDTH+50, (WIN_HEIGHT-50)//2)
+START2_LOCATION = (WIN_WIDTH+50, (WIN_HEIGHT+50)//2)
