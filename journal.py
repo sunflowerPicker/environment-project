cde4c314c8489f25dabee6875f60cedde553a434
avital kucher <avitalk22@gmail.com>
2022-09-21 12:32:32 +0300

    question num list and journal functions

--- journal.py
+++ journal.py
@@ -1,1 +1,18 @@
 import consts
+
+def set_empty_dict():
+    dict_list = {}
+    for i in range(1, 9):
+        dict_list[i] =
+
+info = set_empty_dict()
+
+
+def get_information():
+    return info
+
+
+def set_information(info_dict):
+    info.clear()
+    for x in info_dict:
+        info.update(info_dict)
